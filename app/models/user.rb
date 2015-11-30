f2fd3c10f0f7c7e2fd9e25f2bbad76eab078f318
Justin Hiltz <justin@justinhiltz.com>
2015-11-30 18:14:01 +0000

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accesspr :remember_token
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates(:name, presence: true, length: { maximum: 50 })
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -29,6 +29,12 @@ class User < ActiveRecord::Base
   
   # Returns true if the given token matches the digest
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
